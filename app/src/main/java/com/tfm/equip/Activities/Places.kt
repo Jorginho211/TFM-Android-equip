f8180bb8b3a35f58ac65e3f0d2cd274264537e12
Jorge Lopez <jorge.lopez@riseup.net>
2019-08-12 17:47:29 +0200

    Añadido activity que lista las areas

--- app/src/main/java/com/tfm/equip/Activities/Places.kt
+++ app/src/main/java/com/tfm/equip/Activities/Places.kt
@@ -1,12 +1,32 @@
 package com.tfm.equip.Activities
 
+import android.content.res.Resources
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import com.tfm.equip.R
 
 class Places : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_places)
+
+        var list:ArrayList<String> = ArrayList()
+        list.add("TEst1")
+        list.add("Test2")
+
+        var placesListView:ListView = findViewById(R.id.placesListView)
+        var arrayAdapter: ArrayAdapter<String> = ArrayAdapter(this, android.R.layout.simple_list_item_1, list)
+        placesListView.adapter = arrayAdapter
+
+        placesListView.onItemClickListener = AdapterView.OnItemClickListener{
+            adapterView, view, i, l ->
+            Log.i("Selected", list[i])
+        }
     }
 }
