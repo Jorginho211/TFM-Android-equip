16be0eda9cfc35a03e2adeb7b12a8eeb113d1d33
Jorge Lopez <jorge.lopez@riseup.net>
2019-08-16 11:24:58 +0200

    Fix pedido de permisos sobre bluetooth para iniciar servicio de captura de beacons

--- app/src/main/java/com/tfm/equip/Activities/PrincipalActivity.kt
+++ app/src/main/java/com/tfm/equip/Activities/PrincipalActivity.kt
@@ -117,7 +117,7 @@ class PrincipalActivity: AppCompatActivity() {
 
         when (requestCode) {
             1 -> {
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     stopEquipmentService()
                     startEquipmentService()
                 }
